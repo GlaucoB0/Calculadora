8f919109316854c9293de2cfe638fdcea6515e15
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-05-12 19:12:00 -0300

    laços de repetição

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.calculadora
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.example.calculadora.databinding.ActivityMainBinding
 import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,156 +18,77 @@ class MainActivity : AppCompatActivity() {
 
         val calculo = binding.calculo
         val operadores = listOf('*', '-', '+', '/', '.')
+        val operacaoIds = listOf("multiplicacao", "subtracao","adicao","divisao")
+        var numeros = listOf("zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove")
 
-        binding.igual.setOnClickListener {
+        fun adicionarOperador(operador: String) {
+            val ultimoChar = calculo.text.toString().lastOrNull()
+            if (ultimoChar in operadores) {
+                return
+            }
+            calculo.text = "${calculo.text}$operador"
+        }
 
-            val resultado = Expression(calculo.text.toString()).calculate()
-            if (resultado.isNaN()) {
-                calculo.text = "0"
-            } else {
-                calculo.text = resultado.toString()
+        fun adicionarNumero(numero: String) {
+            if (calculo.text == "0") {
+                calculo.text = ""
+            }
+            calculo.text = "${calculo.text}$numero"
+        }
+        for (numero in numeros) {
+            val buttonId = resources.getIdentifier(numero, "id", packageName)
+            var btn = findViewById<AppCompatButton>(buttonId)
+            btn.setOnClickListener {
+                adicionarNumero(btn.text.toString())
+            }
+        }
+        for (operacao in operacaoIds) {
+            val buttonId = resources.getIdentifier(operacao, "id", packageName)
+            var btn = findViewById<AppCompatButton>(buttonId)
+            btn.setOnClickListener {
+                adicionarOperador(btn.text.toString())
             }
         }
 
+        binding.igual.setOnClickListener {
+            var resultado = Expression(calculo.text.toString()).calculate().toString()
+
+            if (resultado.contains('.') && resultado.endsWith(".0"))
+                calculo.text = "${resultado.dropLast(2)}"
+            else
+                when (resultado) {
+                    "NaN" -> calculo.text = "0"
+                    else -> calculo.text = "${resultado}"
+                }
+        }
         // Apagar
         binding.ce.setOnClickListener {
             calculo.text = "0"
         }
         binding.apagar.setOnClickListener {
-            if (calculo.text.length.equals(1)) {
-                calculo.text = "0"
-            } else {
-                calculo.text = calculo.text.dropLast(1).toString()
-            }
+            if (calculo.text.length == 1) calculo.text = "0"
+            else calculo.text = calculo.text.dropLast(1).toString()
         }
-
-        // Operações
+        // Parenteses
         binding.parenteseAbrindo.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = "("
-            } else {
-                calculo.text = "${calculo.text}("
-            }
+            if (calculo.text == "0") calculo.text = "("
+            else calculo.text = "${calculo.text}("
         }
         binding.parenteseFechando.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ")"
-            } else {
-                calculo.text = "${calculo.text})"
-            }
+            if (!calculo.text.contains('(')) return@setOnClickListener
+            else calculo.text = "${calculo.text})"
         }
-        binding.divisao.setOnClickListener {
-            val ultimoChar = calculo.text.toString().lastOrNull()
-            if (ultimoChar in operadores) {
-                return@setOnClickListener
-            }
-
-            calculo.text = "${calculo.text}/"
-        }
-        binding.multiplicacao.setOnClickListener {
-            val ultimoChar = calculo.text.toString().lastOrNull()
-            if (ultimoChar in operadores) {
-                return@setOnClickListener
-            }
-
-            calculo.text = "${calculo.text}*"
-        }
-        binding.subtracao.setOnClickListener {
-            val ultimoChar = calculo.text.toString().lastOrNull()
-            if (ultimoChar in operadores) {
-                return@setOnClickListener
-            }
-
-            calculo.text = "${calculo.text}-"
-        }
-        binding.adicao.setOnClickListener {
-            val ultimoChar = calculo.text.toString().lastOrNull()
-            if (ultimoChar in operadores) {
-                return@setOnClickListener
-            }
-
-            calculo.text = "${calculo.text}+"
-        }
-
         // Números
         binding.virgula.setOnClickListener {
             val expressao: String = binding.calculo.text.toString()
 
-            if (expressao.lastOrNull() in operadores) {
-                return@setOnClickListener
-            }
+            if (expressao.last() in operadores) return@setOnClickListener
 
             val expressaoAtual = expressao.lastIndexOfAny(charArrayOf('+', '-', '/', '*', ')', '('))
 
-            if (expressao.substring(expressaoAtual + 1).contains('.')) {
+            if (expressao.substring(expressaoAtual + 1).contains('.'))
                 return@setOnClickListener
-            }
-
             calculo.text = "${calculo.text}."
         }
-
-        binding.zero.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = "0"
-            } else {
-                calculo.text = "${calculo.text}0"
-            }
-        }
-        binding.um.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}1"
-        }
-        binding.dois.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}2"
-        }
-        binding.tres.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}3"
-        }
-        binding.quatro.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}4"
-        }
-        binding.cinco.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}5"
-        }
-        binding.seis.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}6"
-        }
-        binding.sete.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}7"
-        }
-        binding.oito.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}8"
-        }
-        binding.nove.setOnClickListener {
-            if (calculo.text.equals("0")) {
-                calculo.text = ""
-            }
-            calculo.text = "${calculo.text}9"
-        }
-
-
     }
 }
